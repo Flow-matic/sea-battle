4ccfa68ba36e71b883988c372e8b468779a4d430
Flow-matic <jonathonvinson22@gmail.com>
2022-02-06 22:19:14 +0000

    Add 5 x 5 board and color

--- run.py
+++ run.py
@@ -1,1 +1,24 @@
+from random import randint
+
+game_board = []
+
+player_one = {
+    "name": "player 1",
+    "wins": 0,
+}
+
+player_two = {
+    "name": "player 2",
+    "wins": 0,
+}
+
+colors = {
+    "reset": "\033[00m",
+    "red": "\003[91m",
+    "blue": "\003[94m",
+    "cyan": "\003[96m",
+}
+
+
+#Building the 5 x 5 board
 
