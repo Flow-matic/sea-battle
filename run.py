6a4aaeaedff6b1770994e0565d471abce1212e87
Flow-matic <jonathonvinson22@gmail.com>
2022-02-06 22:28:33 +0000

    Add ship location row col

--- run.py
+++ run.py
@@ -21,4 +21,30 @@ colors = {
 
 
 #Building the 5 x 5 board
+def build_game_board(board):
+    for item in range(5):
+        board.append(["0"] * 5)
 
+
+def show_board(board):
+    for row in board:
+        print(" ".join(row))
+
+
+# Defining ships locations
+def load_game(board):
+    print("WELCOME TO BATTLESHIP!")
+    print("find and sink the ship!")
+    del board[:]
+    build_game_board(board)
+    print(colors['blue'])
+    show_board(board)
+    print(colors['reset'])
+    ship_col = randint(1, len(board))
+    ship_col = randint(1, len(board[0]))
+    return {
+        'ship_col': ship_col,
+        'ship_row': ship_row,
+    }
+
+    
