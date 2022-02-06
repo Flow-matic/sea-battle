feea2c7e25fa7bb8947fe58daec58f4e7f99e79e
Flow-matic <jonathonvinson22@gmail.com>
2022-02-06 23:06:47 +0000

    Add players imputs and guess

--- run.py
+++ run.py
@@ -81,5 +81,30 @@ def play_again():
 
 
 # What will be done with players guesses
+def input_check(ship_row, ship_col, player, board):
+    guess_col = 0
+    guess_row = 0
+    while True:
 
+        try:
+            guess_row = int(input("Guess Row:")) - 1
+            guess_col = int(input("Guess Col:")) - 1
+        except ValueError:
+
+            print("Enter a number only: ")
+            continue
+        else:
+
+            break
+    match = guess_row == ship_row - 1 and guess_col == ship_col - 1
+    not_on_game_board = (guess_row < 0 \ 
+    or guess_row > 4) or (guess_col < 0 or guess_col > 4)
+
+    if match:
+        player["wins"] += 1
+        print("Congratulations! You sunk my battleship")
+        print('The current match score is %d : %d  (Player1 : Player2)' %
+        (player_one["wins"], player_two["wins"]))
+        print("Thanks for playing!")
+        play_again()
 
